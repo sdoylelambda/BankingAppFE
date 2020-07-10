b3de79004ceafe578c16f65484a341ce635e40cf
Sean Doyle <sdoylelambda@gmail.com>
2020-07-10 16:24:12 -0400

    Nav bar with links/routes, basic styling.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import Nav from './components/Nav'
+import { Link, Switch, Route } from 'react-router-dom'
+import './App.css'
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <h1>hello</h1>
+        {/* <Nav />
+        <h1>hello</h1> */}
+        {/* <Link to='/home' component={Nav}>
+          Home
+        </Link> */}
+        <Link to='/new'>New User</Link>
+        <Link to='/login'>Login</Link>
+        <Link to='/accounts'>Accounts</Link>
+        <Link to='/transfers'>Transfers</Link>
+        <Link to='/faq'>FAQ</Link>
       </header>
+      <Switch>
+        <Route path='/new' component={Nav}>
+          <Nav />
+        </Route>
+      </Switch>
     </div>
   )
 }
